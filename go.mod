ce8ae09e93db8496ed32ba0dcb234634bc674245
Leonid Meleshin <leon.03.99@gmail.com>
2021-02-03 01:33:39 +0300

    feat: display QR code

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/phpdave11/gofpdi v1.0.13 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/signintech/gopdf v0.9.13
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	github.com/ugorji/go v1.2.3 // indirect
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
--- services/booklet/a5.go
+++ services/booklet/a5.go
@@ -2,13 +2,34 @@ package services
 
 import (
 	"github.com/signintech/gopdf"
+	qrcode "github.com/skip2/go-qrcode"
 )
 
 func GenerateBookletA5(pdf *gopdf.GoPdf, theme string, url string) error {
-	tpl := pdf.ImportPage("./assets/templates/a5/"+theme+".pdf", 1, "/MediaBox")
+	{
+		tpl := pdf.ImportPage("./assets/templates/a5/"+theme+".pdf", 1, "/MediaBox")
+		pdf.UseImportedTemplate(tpl, 0, 0, 420, 595)
+	}
 
 	{
-		pdf.UseImportedTemplate(tpl, 0, 0, 430, 595)
+		qr, err := qrcode.New(url, qrcode.Medium)
+		if err != nil {
+			return err
+		}
+
+		qr.DisableBorder = true
+
+		png, err := qr.PNG(256)
+		if err != nil {
+			return err
+		}
+
+		holder, err := gopdf.ImageHolderByBytes(png)
+		if err != nil {
+			return err
+		}
+
+		pdf.ImageByHolderWithOptions(holder, gopdf.ImageOptions{X: 40, Y: 477, Rect: &gopdf.Rect{W: 78, H: 78}})
 	}
 
 	return nil
