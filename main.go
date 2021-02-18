3d2f75ef73c459845150b6cde535f0fd10db7597
Leonid Meleshin <leon.03.99@gmail.com>
2021-02-18 23:33:27 +0300

    feat: add Sentry

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/leon0399/cardyo-pdf/controllers"
 	"rsc.io/quote"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Error loading .env file: %v\n", err)
+	}
+
+	// To initialize Sentry's handler, you need to initialize Sentry itself beforehand
+	if err := sentry.Init(sentry.ClientOptions{}); err != nil {
+		fmt.Printf("Sentry initialization failed: %v\n", err)
+	}
+
 	router := gin.Default()
+
+	router.Use(sentrygin.New(sentrygin.Options{
+		Repanic: true,
+	}))
 
 	router.GET("/quote/go", func(c *gin.Context) {
 		c.String(http.StatusOK, quote.Go())
